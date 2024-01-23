dbe77cae7b645dea8301c5e752133ada2f6412ca
Dabin Son <148966629+sonya030@users.noreply.github.com>
2024-01-23 15:03:25 +0900

    Delete js/subPage.js

--- js/subPage.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let clickableElement = document.querySelector(".goSubpage");
-
-  let targetURL = "../subPage/index.html";
-  let clickHandler = function () {
-    document.location.href = targetURL;
-  };
-  clickableElement.addEventListener("click", clickHandler);
-});
